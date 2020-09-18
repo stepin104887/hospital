1f5d77de09877566ecd562f59025f0b34a901242
stepin104887 <69084828+stepin104887@users.noreply.github.com>
2020-09-18 22:28:31 +0530

    Create main.c

---
+++ project/main.c
@@ -0,0 +1,77 @@
+#include"hospital.h"
+int main()
+{
+    read();
+    int c,i,q;
+    printf("Simple Hospital Management System\n");
+    int m,n;
+//making out the pattern
+    
+    for(m=1; m<=4; m++)
+    {
+        for(n=1; n<=5; n++)
+            printf(" ");
+        for(n=1; n<=m-1; n++)
+        {
+            printf(" ");
+        }
+        for(n=1; n<=4-m+1; n++)
+        {
+            if(n==4-m+1 || m==1 || m==4)
+                printf("*");
+            else
+                printf(" ");
+        }
+        for(n=1; n<=4-m+1; n++)
+        {
+            if(n==1 ||m==1 || m==4)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    while(c!=6)
+    {
+
+        printf("**Enter your choice**\n\n1. Add Information\n2. View Information\n3. Search\n4. Edit Information\n5. Delete Information\n6. Exit\n\nOption=");
+        scanf("%d",&c);//choice for option
+        fflush(stdin);//making it clear
+        if(c==1)//add
+        {
+            system("cls");
+            add();
+        }
+        else if(c==2)//view
+        {
+            system("cls");
+            view();
+        }
+        else if(c==3)//search
+        {
+            system("cls");
+            search();
+        }
+        else if(c==4)//edit
+        {
+            system("cls");
+            edit();
+        }
+        else if(c==5)//delete
+        {
+            system("cls");
+            del();
+        }
+        else if(c==6)
+        {
+            write();
+            return 0;
+        }
+        else
+        {
+            system("cls");
+            printf("\n\nInvalid input , try again by using valid inputs");
+        }
+        printf("\n\n");
+    }
+}
